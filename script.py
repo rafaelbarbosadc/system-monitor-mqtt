3f5160d28a1bea37a97cc72500193458b8848379
Rafael Barbosa <rafaelbarbosadc@gmail.com>
2020-09-07 19:42:50 -0400

    System monitor working

---
+++ script.py
@@ -0,0 +1,24 @@
+import subprocess
+import paho.mqtt.client as mqtt
+from time import sleep
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+def on_message(client, userdata, msg):
+    print("Topic:", msg.topic)
+    payload = msg.payload.decode('utf-8', 'ignore')
+    print(payload)
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("mqtt.eclipse.org", 1883, 60)
+client.subscribe("dse/monitor")
+client.loop_start()
+
+while(True):
+  monitor = subprocess.check_output(['sh', 'monitor.sh'])
+  client.publish("dse/monitor", monitor)
+  sleep(2)
