b3e6a4e1306e8bbda6bbdcc906225efff1420b97
Rafael Barbosa <rafaelbarbosadc@gmail.com>
2020-09-09 16:47:08 -0400

    Change MB to kB

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} {print "CPU Usage: " usage "%"}'
-free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
+free -m | awk 'NR==2{printf "Memory Usage: %s/%skB (%.2f%%)\n", $3,$2,$3*100/$2 }'
 df -m | awk '$NF=="/"{printf "Disk Usage: %d/%dMB (%s)\n", $3,$2,$5}'
 
